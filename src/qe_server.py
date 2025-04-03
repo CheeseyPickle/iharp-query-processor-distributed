cd7471dee19ae22fe272e693349f9148b91b3fa8
Adwin Shi <86745595+CheeseyPickle@users.noreply.github.com>
2025-04-03 13:13:18 -0500

    Release system resources

--- src/qe_server.py
+++ src/qe_server.py
@@ -45,7 +45,8 @@ class DBNode(iharp_query_processor_pb2_grpc.DBNodeServicer):
         # Dump into pickle
         # As long as pickle file is less than ~4.29 GB (2^32 bytes), we can send it
         ds.load()
-        pickled_arr = pickle.dumps(ds) 
+        pickled_arr = pickle.dumps(ds)
+        ds.close()
         return iharp_query_processor_pb2.RasterResponse(pickled_arr=pickled_arr)
 
 
